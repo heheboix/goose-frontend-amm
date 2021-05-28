1562012c0b2da04d7c9548078fdbfd6e7a5fa6b0
heheboix <84672799+heheboix@users.noreply.github.com>
2021-05-28 12:24:22 +0530

    Update config.ts

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -4,93 +4,102 @@ const config: MenuEntry[] = [
   {
     label: 'Home',
     icon: 'HomeIcon',
-    href: 'https://jovial-almeida-d8d825.netlify.app/'
+    href: '/',
   },
   {
     label: 'Trade',
     icon: 'TradeIcon',
-    initialOpenState: true,
     items: [
       {
         label: 'Exchange',
-        href: '/swap'
+        href: 'https://exchange.peachdefi.com/',
       },
       {
         label: 'Liquidity',
-        href: '/pool'
-      }
-    ]
+        href: 'https://exchange.peachdefi.com/#/pool',
+      },
+    ],
   },
-//  {
-  //  label: 'Farms',
-    //icon: 'FarmIcon',
-    //href: 'https://www.goosedefi.com/farms'
- // },
-  //{
-    //label: 'Nests',
-    //icon: 'PoolIcon',
-    //href: 'https://www.goosedefi.com/nests'
- // },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: 'Bowls',
+    icon: 'PoolIcon',
+    href: '/bowls',
+  },
+  // {
+  //   label: 'Pools',
+  //   icon: 'PoolIcon',
+  //   href: '/pools',
+  // },
   // {
   //   label: 'Lottery',
   //   icon: 'TicketIcon',
-  //   href: 'https://www.goosedefi.com/lottery'
+  //   href: '/lottery',
+  // },
+  // {
+  //   label: 'NFT',
+  //   icon: 'NftIcon',
+  //   href: '/nft',
   // },
   {
     label: 'Info',
     icon: 'InfoIcon',
     items: [
-      {
-        label: 'PancakeSwap',
-        href: 'https://pancakeswap.info/token/0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
-      },
-   //   {
-     //   label: 'CoinGecko',
-       // href: 'https://www.coingecko.com/en/coins/goose-finance',
-      //},
-     // {
-       // label: 'CoinMarketCap',
-       // href: 'https://coinmarketcap.com/currencies/goose-finance/',
-      //},
-      //{
-        //label: 'AstroTools',
-        //href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
-      //},
-    ]
+      //  {
+      //    label: 'PancakeSwap',
+      //    href: 'https://pancakeswap.info/token/0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+      //  },
+      //  {
+      //    label: 'CoinGecko',
+      //    href: 'https://www.coingecko.com/en/coins/goose-finance',
+      //  },
+      //  {
+      //    label: 'CoinMarketCap',
+      //    href: 'https://coinmarketcap.com/currencies/goose-finance/',
+      //  },
+      //  {
+      //    label: 'AstroTools',
+      //    href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
+      //  },
+    ],
   },
   {
     label: 'More',
     icon: 'MoreIcon',
     items: [
       {
-        label: "Github",
-        href: "https://github.com/goosedefi/",
+        label: 'Github',
+        href: 'https://github.com/peachdefi/',
       },
       {
-        label: "Docs",
-        href: "https://goosedefi.gitbook.io/goose-finance/",
+        label: 'Docs',
+        href: 'https://peachdefi.gitbook.io/peach-finance/',
       },
-     // {
-       // label: "Blog",
-        //href: "https://goosefinance.medium.com/",
-      //},
+      //  {
+      //    label: 'Blog',
+      //    href: 'https://goosefinance.medium.com/',
+      //  },
     ],
   },
   {
     label: 'Partnerships/IFO',
     icon: 'GooseIcon',
-    href: 'https://docs.google.com/forms/d/e/1FAIpQLSe7ycrw8Dq4C5Vjc9WNlRtTxEhFDB1Ny6jlAByZ2Y6qBo7SKg/viewform?usp=sf_link',
+    href: 'https://forms.gle/Y9B6xEAQe4asakiM8',
   },
-//  {
-  //  label: 'Audit by Hacken',
-    //icon: 'AuditIcon',
-    //href: 'https://www.goosedefi.com/files/hackenAudit.pdf',
-  //},
-  //{
-    //label: 'Audit by CertiK',
-    //icon: 'AuditIcon',
-    //href: 'https://certik.org/projects/goose-finance',
- // },
+  //  {
+  //    label: 'Audit by Hacken',
+  //    icon: 'AuditIcon',
+  //    href: 'https://www.goosedefi.com/files/hackenAudit.pdf',
+  //  },
+  //  {
+  //    label: 'Audit by CertiK',
+  //    icon: 'AuditIcon',
+  //    href: 'https://certik.org/projects/goose-finance',
+  //  },
 ]
 
 export default config
